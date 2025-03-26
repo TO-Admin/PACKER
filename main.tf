1f85d68cd46c98e95b17fc9533fc3bc9d4997d00
Théo <60260648+TO-Admin@users.noreply.github.com>
2025-03-26 15:38:19 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,41 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_security_group" "web_server" {
+  name        = "web_server_sg"
+  description = "Allow HTTP and SSH"
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web_server" {
+  ami           = "ID_DE_VOTRE_AMI" # Remplacez par l'ID de l'AMI créée
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.web_server.id]
+}
+
+output "instance_ip" {
+  value = aws_instance.web_server.public_ip
+}
