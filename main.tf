f0ab1829ae59a165f5318387db9b57dc1a08b062
Théo <60260648+TO-Admin@users.noreply.github.com>
2025-03-27 13:10:06 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,6 +15,14 @@ resource "aws_security_group" "web_server" {
   }
 
   ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     description = "SSH"
     from_port   = 22
     to_port     = 22
