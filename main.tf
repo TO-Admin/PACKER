74c19894a0ebb51cff5927ca6f315f982dc4be9a
Théo <60260648+TO-Admin@users.noreply.github.com>
2025-03-27 13:09:35 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "aws_security_group" "web_server" {
 }
 
 resource "aws_instance" "web_server" {
-  ami           = "ami-040875931bcabfd1a"
+  ami           = ""
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web_server.id]
 }
